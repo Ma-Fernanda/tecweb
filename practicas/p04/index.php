602e53153b2f7fa1ddb4a3729d3365a8f5123404
Ma-Fernanda <mafernandaaguasrojas@gmail.com>
2025-02-02 21:44:05 -0600

    Validación de la variable

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -12,6 +12,7 @@
         echo 'myvar no es variable válida porque no empieza con $ por lo tanto no es aceptable en PHP'. "<br>";
         echo '$myvar es variable válida porque empieza con $ seguida de una letra y algun tipo de nomenclatura aceptable en PHP'. "<br>";
         echo '$var7 es variable válida porque empieza con $ seguida de una letra y algun tipo de nomenclatura aceptable en PHP'. "<br>";
+        echo '$_element1 es variable válida porque empieza con $ seguida de un guión bajo y algun tipo de nomenclatura aceptable en PHP'. "<br>";
 
         
     ?>
