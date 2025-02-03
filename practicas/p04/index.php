729186a441294e4ec34cb1bad97fb0219ed14e05
Ma-Fernanda <mafernandaaguasrojas@gmail.com>
2025-02-02 21:25:54 -0600

    Validación de la variable

---
+++ practicas/p04/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC “-//W3C//DTD XHTML 1.1//EN”
+“http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd”>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title> Variables </title>
+</head>
+<body>
+    <?php
+        echo '$_myvar es variable válida porque empieza con $ seguida de algun tipo de nomenclatura aceptable en PHP' 
+        
+    ?>
+</body>
+</html>
