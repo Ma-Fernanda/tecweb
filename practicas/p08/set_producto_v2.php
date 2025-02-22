b43f8768d31ec1fe64072f0f0554a6fe5823c0fa
Ma-Fernanda <mafernandaaguasrojas@gmail.com>
2025-02-21 22:31:12 -0600

    Modificación de set_producto_v2.php

--- practicas/p08/set_producto_v2.php
+++ practicas/p08/set_producto_v2.php
@@ -26,10 +26,10 @@ if($result->num_rows > 0){
     echo "ERROR: El producto que intenta registrar ya se encuentra en la base de datos.";
 } else {
 
-    $sql = "INSERT INTO productos VALUES (NULL, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}', {$eliminado})";
-    //$sql = "INSERT INTO productos (nombre, marca, modelo, precio, detalles, unidades, imagen) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    //$sql = "INSERT INTO productos VALUES (NULL, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}', {$eliminado})";
+    $sql = "INSERT INTO productos (nombre, marca, modelo, precio, detalles, unidades, imagen) VALUES (?, ?, ?, ?, ?, ?, ?)";
     $stmt = $link->prepare($sql);
-    //$stmt->bind_param("sssdsssi", $nombre, $marca, $modelo, $precio, $detalles, $unidades, $imagen,$eliminado);
+    $stmt->bind_param("sssdsssi", $nombre, $marca, $modelo, $precio, $detalles, $unidades, $imagen,$eliminado);
 
     if ($stmt->execute()) {
         echo "Producto registrado exitosamente.";
@@ -40,7 +40,7 @@ if($result->num_rows > 0){
         echo "<p><strong>Detalles:</strong> $detalles</p>";
         echo "<p><strong>Unidades:</strong> $unidades</p>";
         echo "<p><strong>Imagen:</strong> $imagen</p>";
-        echo "<p><strong>Eliminado:</strong> $eliminado</p>";
+       // echo "<p><strong>Eliminado:</strong> $eliminado</p>";
     } else {
         echo "Error al registrar el producto " . $stmt->error;
     }
