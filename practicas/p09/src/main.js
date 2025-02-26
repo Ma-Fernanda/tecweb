a314f6eb05997cf4169e93e89e337b759067ea8b
Ma-Fernanda <mafernandaaguasrojas@gmail.com>
2025-02-25 19:40:14 -0600

    Ejemplo 18: mostrar nombre de un numero con switch

--- practicas/p09/src/main.js
+++ practicas/p09/src/main.js
@@ -276,5 +276,24 @@ function numCastellano(){
     div1.innerHTML = r;        
 }
 
+//Ejemplo 18
+function numCastellano2(){
+    function convertirCastellano(x) {
+        switch (x) {
+        case 1: return "uno";
+        case 2: return "dos";
+        case 3: return "tres";
+        case 4: return "cuatro";
+        case 5: return "cinco";
+        default: return "valor incorrecto";
+        }
+    }
+    var valor = prompt("Ingresa un valor entre 1 y 5 :", "");
+        valor = parseInt(valor);
+        var r = convertirCastellano(valor);
+        var div1 = document.getElementById('castellano');
+        div1.innerHTML = r;  
+    }
+    
 
 
