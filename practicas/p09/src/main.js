46f5fded75af45cda041a104608c7fee8e58ecb0
Ma-Fernanda <mafernandaaguasrojas@gmail.com>
2025-02-25 19:39:23 -0600

    Ejemplo 17: mostrar nombre de un numero con if-else return

--- practicas/p09/src/main.js
+++ practicas/p09/src/main.js
@@ -249,4 +249,32 @@ function rangoNum(){
     mostrarRango(valor1, valor2);
 }
 
+//Ejemplon 17
+function numCastellano(){
+    function convertirCastellano(x) {
+        if(x==1)
+        return 'uno';
+        else
+        if(x==2)
+        return 'dos';
+        else
+        if(x==3)
+        return 'tres';
+        else
+        if(x==4)
+        return 'cuatro';
+        else
+        if(x==5)
+        return 'cinco';
+        else
+        return 'valor incorrecto';     
+    }
+    var valor = prompt("Ingresa un valor entre 1 y 5 :", "");
+    valor = parseInt(valor);
+    var r = convertirCastellano(valor);
+    var div1 = document.getElementById('cast');
+    div1.innerHTML = r;        
+}
+
+
 
