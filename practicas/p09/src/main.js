9646fa4db2874d281fb92240187fa3f88a53091b
Ma-Fernanda <mafernandaaguasrojas@gmail.com>
2025-02-25 19:22:46 -0600

    Ejemplo 2: mostrar datos definidos en las variables

--- practicas/p09/src/main.js
+++ practicas/p09/src/main.js
@@ -16,5 +16,21 @@ function holamundo(){
     document.getElementById('ejemplo1').innerText = 'Hola Mundo';
 }
 
+//Ejemplo 2
+function datos() {
+    var nombre = 'Juan';
+    var edad = 10;
+    var altura = 1.92;
+    var casado = false;
+    var div1 = document.getElementById('nombre1');
+    div1.innerText = 'Nombre: '+ nombre ;
+    var div2 = document.getElementById('edad1');
+    div2.innerText = 'Edad: ' +  edad;
+    var div3 = document.getElementById('altura');
+    div3.innerText = 'Altura: ' +  altura;
+    var div4 = document.getElementById('casado');
+    div4.innerText = 'Casado: ' +  casado;
+}
+
 
 
