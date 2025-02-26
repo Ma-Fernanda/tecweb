fe84ff11578af0f750ee4fd4045a5685bfc710a4
Ma-Fernanda <mafernandaaguasrojas@gmail.com>
2025-02-25 19:21:00 -0600

    Ejemplo 1: mostrar un hola mundo

--- practicas/p09/src/main.js
+++ practicas/p09/src/main.js
@@ -1,4 +1,4 @@
-
+/*
 function getDatos(){
     var nombre = prompt("Nombre: ", "");
 
@@ -10,4 +10,11 @@ function getDatos(){
     var div2 = document.getElementById('edad');
     div2.innerHTML = '<h3> Edad: '+edad+'</h3>';
 }
+*/
+//Ejemplo 1
+function holamundo(){
+    document.getElementById('ejemplo1').innerText = 'Hola Mundo';
+}
+
+
 
