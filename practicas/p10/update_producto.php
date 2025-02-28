20f91cc86084547e603ed203d9ad9ffedb3f7b90
Ma-Fernanda <mafernandaaguasrojas@gmail.com>
2025-02-28 01:49:12 -0600

    ejercicio 4: archivo update que modifica datos

--- practicas/p10/update_producto.php
+++ practicas/p10/update_producto.php
@@ -1,35 +1,61 @@
 <?php
 $link = mysqli_connect("localhost", "root", "Fernanda465", "marketzone");
 
-if($link === false){
+if ($link === false) {
     die("ERROR: No pudo conectarse con la DB. " . mysqli_connect_error());
 }
 
-if(isset($_POST['id']) && isset($_POST['nombre']) && isset($_POST['descripcion']) && isset($_POST['precio'])){
-    $id = $_POST['id']; 
-    $nombre = $_POST['nombre'];
-    $marca = $_POST['marca'];
-    $modelo = $_POST['modelo'];
-    $precio = $_POST['precio'];
-    $detalles =$_POST['details'];
-    $unidades = $_POST['unidades'];
-    $imagen   = $_POST['imagen'];
-    $eliminado = 0;
 
-    $precio = floatval($precio);
-    $unidades = intval($unidades);
-    
-    $sql = "UPDATE productos SET nombre='$nombre', marca='$marca', modelo='$modelo',precio=$precio, details='$detalles', unidades=$unidades, imagen='$imagen' WHERE id=$id";
-    if(mysqli_query($link, $sql)){
-        echo "Producto actualizado.";
-        echo '<a href="get_productos_xhtml_v2.php">Ver todos los productos</a> | ';
-        echo '<a href="get_productos_vigentes_v2.php">Ver productos vigentes</a>';
-    } else {
-        echo "ERROR: No se ejecutó $sql. " . mysqli_error($link);
+
+    $updates = [];
+
+    if (isset($_POST['nombre']) && $_POST['nombre'] != '') {
+        $nombre = $_POST['nombre'];
+        $updates[] = "nombre='$nombre'";
     }
-} else {
-    echo "ERROR: Falta información para actualizar el producto.";
-}
+
+    if (isset($_POST['marca']) && $_POST['marca'] != '') {
+        $marca = $_POST['marca'];
+        $updates[] = "marca='$marca'";
+    }
+
+    if (isset($_POST['modelo']) && $_POST['modelo'] != '') {
+        $modelo = $_POST['modelo'];
+        $updates[] = "modelo='$modelo'";
+    }
+
+    if (isset($_POST['precio']) && $_POST['precio'] != '') {
+        $precio = floatval($_POST['precio']);
+        $updates[] = "precio=$precio";
+    }
+
+    if (isset($_POST['details']) && $_POST['details'] != '') {
+        $detalles = $_POST['details'];
+        $updates[] = "details='$detalles'";
+    }
+
+    if (isset($_POST['unidades']) && $_POST['unidades'] != '') {
+        $unidades = intval($_POST['unidades']);
+        $updates[] = "unidades=$unidades";
+    }
+
+    if (isset($_POST['imagen']) && $_POST['imagen'] != '') {
+        $imagen = $_POST['imagen'];
+        $updates[] = "imagen='$imagen'";
+    }
+
+    if (count($updates) > 0) {
+        $sql = "UPDATE productos SET " . implode(', ', $updates) . " WHERE id=$id";
+        if (mysqli_query($link, $sql)) {
+            echo "Producto actualizado.";
+            echo '<a href="get_productos_xhtml_v2.php">Ver todos los productos</a> | ';
+            echo '<a href="get_productos_vigentes_v2.php">Ver productos vigentes</a>';
+        } else {
+            echo "ERROR: No se ejecutó $sql. " . mysqli_error($link);
+        }
+    } else {
+        echo "ERROR: No se enviaron datos para actualizar el producto.";
+    }
 
 mysqli_close($link);
 ?>
