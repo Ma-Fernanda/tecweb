6167af551beeef748c01d4efdacf0eabbc776b63
Ma-Fernanda <mafernandaaguasrojas@gmail.com>
2025-04-11 00:05:36 -0600

    index.php con paso de parámetros realizado y .htaccess creado

--- practicas/p16/pruebaslim/index.php
+++ practicas/p16/pruebaslim/index.php
@@ -11,5 +11,11 @@ $app->get('/', function ($request, $response, $args){
     $response->getBody()->write("Hola mundo Slim");
     return $response;
 });
+
+$app->get("/hola[/{nombre}]", function($request, $response, $args){
+    $response->getBody()->write("Hola, ". $args["nombre"]);
+    return $response;
+});
+
 $app->run();
 ?>
